1bd15e11cc496a3f59c6d96672efe56328af6fef
Benjamin Hughes <behu@kea.dk>
2021-02-04 15:37:54 +0100

    new shit

---
+++ src/main/java/thymeleaf/Thymeleaf.java
@@ -0,0 +1,11 @@
+package thymeleaf;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+
+@SpringBootApplication
+public class Thymeleaf {
+    public static void main(String[] args) {
+        SpringApplication.run(Thymeleaf.class, args);
+    }
+}
---
+++ src/main/java/thymeleaf/controllers/Product.java
@@ -0,0 +1,37 @@
+package thymeleaf.controllers;
+
+import com.example.demo.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+@Controller
+public class Product {
+
+    // using a simple template
+    @GetMapping("product")
+    private String product() {
+        return "products/product";
+    }
+
+
+
+    // using variable
+    @GetMapping("product-variables")
+    private String product(Model model) {
+        thymeleaf.models.Product tv = new thymeleaf.models.Product("tv", 3000);
+        thymeleaf.models.Product mobile = new thymeleaf.models.Product("mobile", 7000);
+
+        ArrayList<thymeleaf.models.Product> products = new ArrayList<thymeleaf.models.Product>();
+
+        products.add(tv);
+        products.add(mobile);
+
+        model.addAttribute("pageTitle", "Buy awesome products");
+        model.addAttribute("products", products);
+        return "products/product-variables";
+    }
+}
